173eb5ba644e2d452c00f0b40fc3a4c70c8ea416
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-07-02 18:38:09 -0400

    started pseudocode

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// Display today's date in #currentDay (moment() with format for day of the week, month, day, and year)
+
+// perform above function automatically every 6 hours to keep #currentDay accurate 
+
+// Time blocks are color-coded to indicate whether task is in the past, present, or future (audit tasks). 
+
+// Create timer function that reaudits tasks every 30 minutes
+
+// Click on a time block to edit and enter an event (replace element with input)
+
+// Save task in localStorage when clicking the save button for that time block (save onclick setItem)
