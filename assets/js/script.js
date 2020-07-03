1189f3f760d6ac5aa0e883f051b2285549a096ce
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-07-03 17:49:15 -0400

    timeblocks and currentDay are now audited every minute

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,27 @@
 // Display today's date in #currentDay (moment() with format for day of the week, month, day, and year)
-var currentDay = moment().format("dddd, MMMM Do, YYYY");
-$("#currentDay").text(currentDay);
+function renderCurrentDay() {
+    var currentDay = moment().format("dddd, MMMM Do, YYYY");
+    $("#currentDay").text(currentDay).
+        attr("data-day", moment().format("D"));
+}
 
-// perform above function automatically every 6 hours to keep #currentDay accurate 
+// perform above function automatically every 6 hours to keep #currentDay accurate and clear task lists on new days
+function auditCurrentDay() {
+    // get day of month
+    var dayOfMonth = moment().format("D");
+
+    // check if date has changed
+    if ($("#currentDay").data("day") !== dayOfMonth) {
+        // clear all timeblocks
+        $(".description").each(function(){
+          $(this).text("");
+        });
+
+        // render new date
+        renderCurrentDay();
+    }
+}
+
 
 // Time blocks are color-coded to indicate whether task is in the past, present, or future (audit tasks). 
 function auditTasks() {
@@ -13,23 +32,31 @@ function auditTasks() {
     $(".description").each(function(){
         var dataHour = $(this).data("hour");
 
-        // if hour block is in the past
+        // if time block is in the past
         if (currentHour > dataHour) {
             $(this).addClass("past");
         }
+        // if time block is current
         else if (currentHour == dataHour) {
             $(this).addClass("present");
         }
+        // if time block is in the future
         else  {
             $(this).addClass("future");
         }
     });
 }
 
-// Create timer function that reaudits tasks every 30 minutes
-
 // Click on a time block to edit and enter an event (replace element with input)
 
-// Save task in localStorage when clicking the save button for that time block (save onclick setItem)
+// replace text input with div Save task in localStorage when clicking the save button for that time block (savebtn onclick setItem)
 
+// load tasks from localStorage when page refreshes (getItem)
+
+
+renderCurrentDay();
 auditTasks();
+
+// audit date and tasks every minute
+setInterval(auditCurrentDay, (1000 * 60));
+setInterval(auditTasks, (1000 * 60));
